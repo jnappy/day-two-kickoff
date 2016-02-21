47362755363ba7e0de5081d0ca0d7291517563e6
jnappy <jpnappy@gmail.com>
2016-02-21 11:53:41 -0500

    Update exercise.rb

--- exercise.rb
+++ exercise.rb
@@ -1,14 +1,17 @@
 require 'pry'
 
 
-#create a first method called clean_full_name that accepts two parameters: first_name, last_name
+#create a method called clean_full_name that accepts two parameters: first_name, last_name
 #this method should make all the letters of both those paramters lowercase and then capitalize the first letter of each
-#this method should return the combination of the first and last name as one string
+#this method should print to the screen a combination of the first and last name as one string
 
 #ask the user for their first name
 
+# store it in a variable called "user_first_name"
+
 #ask the user for their last name
 
-#run the first method and store the results in a variable called full_name
+# store it in a variable called "user_last_name"
 
-#print the full_name to the screen
+#run the function
+
