b68bcb997b2539a9f40fb5e7455f2c8f119f8ecc
Jay <jpnappy@gmail.com>
2016-02-21 09:54:50 -0500

    first commit

---
+++ exercise.rb
@@ -0,0 +1,14 @@
+require 'pry'
+
+
+#create a first method called clean_full_name that accepts two parameters: first_name, last_name
+#this method should make all the letters of both those paramters lowercase and then capitalize the first letter of each
+#this method should return the combination of the first and last name as one string
+
+#ask the user for their first name
+
+#ask the user for their last name
+
+#run the first method and store the results in a variable called full_name
+
+#print the full_name to the screen
