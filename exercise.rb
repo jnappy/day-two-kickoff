3018eff1a10cb79aa96234ecd023534af1a8d557
sivancie <s.ivancie@gmail.com>
2016-02-21 12:39:43 -0500

    Ruby function

--- exercise.rb
+++ exercise.rb
@@ -1,12 +1,23 @@
 require 'pry'
+def clean_full_name(first_name, last_name)
+    puts (first_name.downcase.capitalize + " " + last_name.downcase.capitalize)
+  end
 
+puts("what is your first name")
+
+user_first_name = gets.strip
+
+puts("what is your last name")
+
+user_last_name = gets.strip
+
+clean_full_name(user_first_name, user_last_name)
 
 #create a method called clean_full_name that accepts two parameters: first_name, last_name
 #this method should make all the letters of both those paramters lowercase and then capitalize the first letter of each
 #this method should print to the screen a combination of the first and last name as one string
 
 #ask the user for their first name
-
 # store it in a variable called "user_first_name"
 
 #ask the user for their last name
@@ -14,4 +25,3 @@ require 'pry'
 # store it in a variable called "user_last_name"
 
 #run the function
-
