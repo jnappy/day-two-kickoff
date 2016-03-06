675033e71327dd4119289c2e4b940979614e0e7c
James Eubank <jameseubank@Jamess-MacBook-Pro.local>
2016-03-06 10:29:11 -0500

    Completed the day two exercise

--- exercise.rb
+++ exercise.rb
@@ -5,13 +5,16 @@ require 'pry'
 #this method should make all the letters of both those paramters lowercase and then capitalize the first letter of each
 #this method should print to the screen a combination of the first and last name as one string
 
+def clean_full_name(first_name,last_name)
+  puts "Your clean full name is: #{first_name.downcase.capitalize} #{last_name.downcase.capitalize}"
+end
 #ask the user for their first name
-
+puts "Hey user, give me your first name."
 # store it in a variable called "user_first_name"
-
+first_name = gets.strip
 #ask the user for their last name
-
+puts "Hey user, give me your last name."
 # store it in a variable called "user_last_name"
-
+last_name = gets.strip
 #run the function
-
+clean_full_name(first_name,last_name)
