900605a536bc62459660cb1858e4179d4cc86e14
Dev Eggers <deveggers@Brendans-MacBook-Pro.local>
2016-03-06 10:43:09 -0500

    Created a full_name variable that saved the input of clean_full_name and puts it to the screen

--- exercise.rb
+++ exercise.rb
@@ -5,9 +5,8 @@ require 'pry'
 #this method should make all the letters of both those paramters lowercase and then capitalize the first letter of each
 #this method should print to the screen a combination of the first and last name as one string
 def clean_full_name(first_name,last_name)
-  puts "First Name: #{first_name.downcase.capitalize} \n Last Name: #{last_name.downcase.capitalize}"
+  "#{first_name.downcase.capitalize} #{last_name.downcase.capitalize}"
 end
-
 #ask the user for their first name
 puts "What is the First Name?"
 # store it in a variable called "user_first_name"
@@ -18,3 +17,7 @@ puts "What is the Last Name?"
 user_last_name = gets.strip
 #run the function
 clean_full_name(user_first_name, user_last_name)
+
+
+full_name = clean_full_name(user_first_name,user_last_name)
+puts "This is the full name: #{full_name}"
